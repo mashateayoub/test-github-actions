2af8ebed564b2b8e25aed966565ef9e48f86ebad
mashateayoub <mshtayub@gmail.com>
2026-02-06 10:55:50 +0100

    Ajout de la configuration des tests dans le module de test et mise à jour des dépendances dans turbo.json et package.json

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -8,7 +8,13 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: 'DRIZZLE',
+          useValue: {},
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
